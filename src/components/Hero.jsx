d233ce425c057e8c014d7c455b920ea5f8e763f3
diegojojoayandun <xcryptorlabs@gmail.com>
2025-02-01 15:17:57 -0500

    Actualizado nombre del sitio

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -92,7 +92,7 @@ const Hero = () => {
           <img
             className="absolute bottom-0 ml-[50vw]
             lg:ml-[75vw] md:ml-[60vw] xmd:ml-[60vw] 2xl:ml-[83vw]
-            sm:h-[90vh] md:h-[70vh] xl:h-[80vh]"
+            sm:h-[90vh] md:h-[70vh] xl:h-[80vh] loading:lazy"
             src={diego}
             alt="diegojojoa"
           />
